c55be1e642045b6e0fa9b07f1b0ca8a5c696ea11
hhx <709866060@qq.com>
2020-10-19 23:51:00 +0800

    update

--- Algorithm4/sort/BaseSort.ts
+++ Algorithm4/sort/BaseSort.ts
@@ -7,19 +7,12 @@ export default abstract class BaseSort<T>  {
 
   protected exchange(arr: Array<T>, index: number, jndex: number) {
     if (index !== jndex) {
-      [arr[index],arr[jndex]] = [arr[jndex],arr[index]]
+      [arr[index],arr[jndex]] = [arr[jndex],arr[index]];
     }
-    return arr
+    return arr;
   }
 
   protected show(arr: Array<T>) {
-    console.log(JSON.stringify(arr))
+    console.log(JSON.stringify(arr));
   }
-
-  // protected isSorted(arr: Array<T>) {
-  //   for (let index = 1; index < arr.length; index++) {
-  //     if (this.less(arr[index], arr[index-1])) return false
-  //   }
-  //   return true
-  // }
 }
--- Algorithm4/sort/SelectionSort.ts
+++ Algorithm4/sort/SelectionSort.ts
@@ -1,6 +1,11 @@
-import BaseSort from "./BaseSort";
+import BaseSort from './BaseSort';
 
 /**
+ * 选择排序：首先找到数组中最小的元素，其次，将它和数组的第一个元素交换位置。
+ * 再次：在剩下的元素中找到最小的元素，将他和数组中第二个元素交换位置。
+ * ...
+ *
+ *
  * 运行时间和输入无关。
  * 数据移动是最少的。
  *
@@ -12,14 +17,14 @@ export default class SelectionSort extends BaseSort<number> {
     for (let index = 0; index < N; index++) { // N
       let min = index; // 最小元素的索引
       for (let j = index + 1; j < N; j++) {  // 迭代当前元素后面的子列
-        if (this.less(arr[j], arr[min])) min = j // 找到最小值（min）下标
+        if (this.less(arr[j], arr[min])) min = j; // 找到最小值（min）下标
       }
       this.exchange(arr, index, min); // 交换第一个数和最小值。
     }
-    return arr
+    return arr;
   }
 
   less(a: number, b: number) {
-    return a < b
+    return a < b;
   }
 }
