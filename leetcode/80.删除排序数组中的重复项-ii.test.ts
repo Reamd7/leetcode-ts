35d9444ffa3f0f540d6a61d605a23f9c6013d0f4
reamd7 <reamd7@gmail.com>
2021-01-02 14:16:42 +0800

    update

--- leetcode/80.删除排序数组中的重复项-ii.test.ts
+++ leetcode/80.删除排序数组中的重复项-ii.test.ts
@@ -16,4 +16,11 @@ describe('删除排序数组中的重复项-ii', () => {
     expect(main(nums)).toEqual(N);
     expect(nums.slice(0, N)).toEqual(res);
   });
+  it('给定 nums = [0,1,1,1,2,2,3,3,4,4,100]', () => {
+    const nums = [0, 1, 1, 1, 2, 2, 3, 3, 4, 4, 100];
+    const res = [0, 1, 1, 2, 2, 3, 3, 4, 4, 100];
+    const N = res.length;
+    expect(main(nums)).toEqual(N);
+    expect(nums.slice(0, N)).toEqual(res);
+  });
 });
--- leetcode/80.删除排序数组中的重复项-ii.ts
+++ leetcode/80.删除排序数组中的重复项-ii.ts
@@ -6,7 +6,7 @@
 
 // @lc code=start
 function removeDuplicates(nums: number[]): number {
-  const windowLength = 2; // 可变滑动窗口大小
+  const windowLength = 2; // 可变滑动窗口大小 => 使得每个元素最多出现两次
 
   let count = 1;
   let subArrayCur = 0; // 交换的下标
@@ -14,28 +14,48 @@ function removeDuplicates(nums: number[]): number {
 
   for (let index = 1; index < nums.length; index++) {
     const element = nums[index];
-    if (element > target) {
+    if (element > target) { // update new change value
       target = element;
-      subArrayCur++; //加一个交换的下标
-      nums[index] = nums[subArrayCur];
-      nums[subArrayCur] = element;
       count = 1;
-    } else if (element == target) {
+      subArrayCur += 1; //加一个交换的下标
+      // ========================================
+      if (index !== subArrayCur) { // [0, 1, 2, 3, 4, 5]
+        nums[index] = nums[subArrayCur];
+        nums[subArrayCur] = element;
+      }
+    } else { // continue same change value
       // 滑动窗口
       if (count < windowLength) {
-        count++;
-        subArrayCur++; //加一个交换的下标
-        nums[index] = nums[subArrayCur];
-        nums[subArrayCur] = element;
-      }
-    } else {
-      if (count < windowLength) {
-        count++;
-        subArrayCur++; //加一个交换的下标
-        nums[index] = nums[subArrayCur];
-        nums[subArrayCur] = element;
+        count += 1;
+        subArrayCur += 1; //加一个交换的下标
+        // ========================================
+        if (index !== subArrayCur) {
+          nums[index] = nums[subArrayCur];
+          nums[subArrayCur] = element;
+        }
       }
     }
+
+    // 排序好了就不会有这个可能性了
+    // else if (element == target) { // continue same change value
+    //   // 滑动窗口
+    //   if (count < windowLength) {
+    //     count++;
+    //     subArrayCur++; //加一个交换的下标
+    //     changeIndex(nums, index, subArrayCur);
+    //     // nums[index] = nums[subArrayCur];
+    //     // nums[subArrayCur] = element;
+    //   }
+    // } else {
+    //   if (count < windowLength) {
+    //   count++;
+    //   subArrayCur++; //加一个交换的下标
+    //   changeIndex(nums, index, subArrayCur);
+
+    //   nums[index] = nums[subArrayCur];
+    //   nums[subArrayCur] = element;
+    //   }
+    // }
 
   }
   return subArrayCur + 1;
