819ced7afdb23490dc96f42f1ceccb04c901db2d
reamd7 <reamd7@gmail.com>
2021-01-13 01:50:00 +0800

    update 445

--- leetcode/445.两数相加-ii.ts
+++ leetcode/445.两数相加-ii.ts
@@ -21,27 +21,83 @@ interface ListNode {
   next: ListNode | null
 }
 
+// function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | null {
+//   let str = '';
+//   let str1 = '';
+//   let cu: ListNode | null = l1;
+//   while (cu !== null) {
+//     str = str + String(cu.val);
+//     cu = cu.next;
+//   }
+//   cu = l2;
+//   while (cu !== null) {
+//     str1 = str1 + String(cu.val);
+//     cu = cu.next;
+//   }
+//   // eslint-disable-next-line no-undef
+//   const res = String(BigInt(str) + BigInt(str1));
+//   const d: ListNode = {
+//     val: NaN,
+//     next: null,
+//   };
+//   cu = d;
+//   for (let index = 0; index < res.length; index++) {
+//     const node = {
+//       val: Number(res[index]),
+//       next: null,
+//     };
+//     cu.next = node;
+//     cu = node;
+//   }
+//   return d.next;
+// };
 function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | null {
-  let str = '';
-  let str1 = '';
+  const str: number[] = [];
+  const str1: number[] = [];
   let cu: ListNode | null = l1;
   while (cu !== null) {
-    str = str + String(cu.val);
+    str.unshift(cu.val);
     cu = cu.next;
   }
   cu = l2;
   while (cu !== null) {
-    str1 = str1 + String(cu.val);
+    str1.unshift(cu.val);
     cu = cu.next;
   }
   // eslint-disable-next-line no-undef
-  const res = String(BigInt(str) + BigInt(str1));
+  let plus = 0;
+  const s_len = str.length;
+  const s1_len = str1.length;
+  let res: number[];
+
+  if (s_len > s1_len) {
+    for (let index = 0; index < s_len; index++) {
+      const a = str[index];
+      const b = str1[index] || 0;
+      const s = a + b + plus;
+      plus = s > 9 ? 1 : 0;
+      str[index] = plus ? s - 10 : s;
+    }
+    res = str;
+  } else {
+    for (let index = 0; index < s1_len; index++) {
+      const a = str[index] || 0;
+      const b = str1[index];
+      const s = a + b + plus;
+      plus = s > 9 ? 1 : 0;
+      str1[index] = plus ? s - 10 : s;
+    }
+    res = str1;
+  }
+  if (plus === 1) {
+    res.push(1);
+  }
   const d: ListNode = {
     val: NaN,
     next: null,
   };
   cu = d;
-  for (let index = 0; index < res.length; index++) {
+  for (let index = res.length - 1; index > -1; index--) {
     const node = {
       val: Number(res[index]),
       next: null,
